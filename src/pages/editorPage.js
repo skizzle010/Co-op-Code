21f64e2eca11422d55f03bc640df29a4bfae6693
skizzle010 <raiutkarsh3219@gmail.com>
2024-07-23 05:07:23 +0530

    socket server

--- src/pages/editorPage.js
+++ src/pages/editorPage.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Client from './components/client';
 import Editor from "./components/editor";
-
+import { initSocket } from '../socket';
+import ACTIONS from '../Actions';
+import { useLocation } from 'react-router-dom';
 
 const EditorPage = () => {
+
+    const socketRef = useRef(null);
+    const location = useLocation();
+    useEffect(() => {
+        const init = async () =>{
+            socketRef.current = await initSocket();
+            socketRef.current.emit(ACTIONS.JOIN,{
+                roomId,
+                username: location.state?.username,
+            });
+
+        }
+        init();
+    },[]
+
+)
 
     const [clients, setClients] = useState([
         {socketId:1,username:'utkarsh Rai'},
